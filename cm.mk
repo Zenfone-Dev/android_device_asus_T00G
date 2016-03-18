c62dc2af4d9e19fb0f478bd3f6f11e9759bb9980
artefvck <artefvck07@gmail.com>
2016-03-18 19:09:27 +0700

    Initial commit

---
+++ cm.mk
@@ -0,0 +1,13 @@
+-include device/asus/T00F/cm.mk
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := cm_T00G
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := ASUS_T00G
+PRODUCT_MANUFACTURER := asus
+PRODUCT_DEVICE := T00G
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=WW_a600cg \
+    BUILD_FINGERPRINT=asus/WW_a600cg/T00G:5.0/LRX21V/WW_user_3.24.40.87_20151222_34:user/release-keys \
+    PRIVATE_BUILD_DESC="a600cg-user 5.0 LRX21V WW_user_3.24.40.87_20151222_34 release-keys"
